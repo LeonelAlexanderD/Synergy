27c4372bdab8821acb5c27c8f1aeca344c664fcb
Alex Diaz <chaffs25@gmail.com>
2024-04-30 21:04:49 -0300

    correccion de ruta de imagen, se añade el boton back to top

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+/* Frase aparece y desaparece */
 
 const app= document.getElementById('typewriter');
 
@@ -13,3 +13,20 @@ typewriter
 .pauseFor(400)
 .start();
 
+/** boton back to top */
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
