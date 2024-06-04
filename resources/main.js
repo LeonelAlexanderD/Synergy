edf7032a145be5ddd92a492c30b0b08467a805fa
Alex Diaz <chaffs25@gmail.com>
2024-06-04 18:00:35 -0300

    validacion de campos en el form con js, consumo de WeatherApi

---
+++ resources/main.js
@@ -0,0 +1,75 @@
+
+/* Frase aparece y desaparece */
+
+const app= document.getElementById('typewriter');
+
+const typewriter = new Typewriter(app, {
+    loop: true,
+    delay:60
+});
+
+typewriter
+.typeString('Veni a conocer el Poncho!')
+.pauseFor(400)
+.start();
+
+/** boton back to top */
+let mybutton = document.getElementById("backtop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if ( document.documentElement.scrollTop > 20) { /** document.body.scrollTop > 20 || */
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  // document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+
+(function () {
+  'use strict'
+
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+
+//api WeatherApi
+const fetchWeatherData = () => {
+  const url = `https://api.weatherapi.com/v1/current.json?key=ee411c5bd3494519a75200638240406&q=Catamarca&aqi=no&lang=es`;
+
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const currentTemp = data.current.temp_c;
+      const weatherDescription = data.current.condition.text;
+
+      const temperatureElement = document.getElementById('temperatura');
+      temperatureElement.innerHTML = `
+        <h2>Temperatura actual: ${currentTemp} °C </h2>
+        <p class= "text-primary fs-5" >Descripción: ${weatherDescription}</p>
+      `;
+    })
+    .catch(error => {
+      console.error('Error al obtener datos del clima:', error);
+    });
+};
+fetchWeatherData();
